9b4f6ebb7993a1f5a5e685d9cac3db0a6b7f23a3
Parv <ninamaparv@gmail.com>
2025-07-14 01:04:27 +0530

    Add Home Button and music button

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ let cells = document.querySelectorAll(".cells");
 let msg = document.querySelector(".msg");
 let msgContainer = document.querySelector(".msg-container");
 let resetGameButton = document.querySelector(".reset");
+let homeButton = document.querySelector(".home");
+let themeButton = document.querySelector(".theme");
+let musicButton = document.querySelector(".music");
 
 let gameMode = "multi"; 
 let turnO = true; //For player O
 let turnCount = 0;
+const audio = new Audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+audio.loop =true
+const wait = (ms)=> new Promise(resolve => setTimeout(resolve,ms));
 const winConditions = [
     [0,1,2],
     [0,3,6],
@@ -33,13 +39,18 @@ function startGame(mode) {
 const resetGame = ()=>{
     turnO = false;
     turnCount = 0;
-    enableButton();
+    enableButtons();
     resetGameButton.innerText = "Reset Game";
+    msg.style.textDecoration = "none";
     msg.innerText = gameMode === "multi" ? "Player X's Turn" : "Player's Turn";
+    for(let cell of cells){
+            cell.innerText = "";
+            cell.classList.remove("win", "O", "X");
+    }
 }
 
 cells.forEach((cell) =>{
-    cell.addEventListener("click",()=>{
+    cell.addEventListener("click", async ()=>{
         if(cell.innerText != "") return;
         
         if(gameMode == "multi"){
@@ -48,10 +59,13 @@ cells.forEach((cell) =>{
         else if(gameMode == "single" && !turnO){
             msg.innerText = "Player's Turn";
             playerMove(cell);
+            disableButtons();
 
             if(turnCount < 9){
                 msg.innerText = "Computer's Turn";
-                setTimeout(computerMove,400);
+                await wait(400);
+                computerMove();
+                enableButtons();
             }
         }
 
@@ -71,6 +85,7 @@ const playerMove = (cell)=>{
         cell.classList.add("X");
         turnO = true;
     }
+    cell.style.pointerEvents = "none";
     cell.disabled = true;
     turnCount++;
     checkWinner();
@@ -154,26 +169,25 @@ const checkResult = ()=>{
     return null;
 }
 
-const enableButton = ()=>{
+const enableButtons = ()=>{
     for(let cell of cells){
+        cell.style.pointerEvents = "auto";
         cell.disabled = false;
-        cell.innerText = "";
-        cell.classList.remove("win");
-        cell.classList.remove("O");
-        cell.classList.remove("X");
     }
 }
 
-const disableButton = ()=>{
+const disableButtons = ()=>{
     for(let cell of cells){
+        cell.style.pointerEvents = "none";
         cell.disabled = true;
     }
 }
 
 const showWinner = (winner)=>{
     msg.innerText = gameMode === "multi" ? `Player ${winner} wins!` : " Computer Wins";
+    msg.style.textDecoration = "underline";
     resetGameButton.innerText = "New Game";
-    disableButton();
+    disableButtons();
 }
 
 const WinningLine = (condition)=>{
@@ -200,10 +214,30 @@ const checkWinner = ()=>{
     }
     if (turnCount === 9 && winnerFound == false) {
         msg.innerText = "It's a Draw!";
-        msgContainer.style.display = "block";
-        resetGameButton.classList.add('hide');
-        disableButton();
+        resetGameButton.innerText = "New Game";
+        disableButtons();
     }
 } 
 
 resetGameButton.addEventListener("click", resetGame);
+
+homeButton.addEventListener("click", ()=>{
+    homeScreen.style.display = "block";
+    gameScreen.style.display = "none";
+    resetGame();
+});
+
+musicButton.addEventListener("click",()=>{
+    const musicIcon = musicButton.querySelector("i");
+    if(musicIcon.classList.contains("fa-volume-xmark")){
+        musicIcon.classList.remove("fa-volume-xmark");
+        musicIcon.classList.add("fa-volume-high");
+        audio.play();
+    }
+    else{
+        musicIcon.classList.remove("fa-volume-high");
+        musicIcon.classList.add("fa-volume-xmark");
+        audio.pause(); 
+        audio.currentTime = 0;
+    }
+});
